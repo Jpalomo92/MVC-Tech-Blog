3954383e163e0125f82d3ac08f90a0b498a6caf7
Jpalomo <Jpalomo92@gmail.com>
2024-03-03 22:03:11 -0600

    added the posted data to be rendered

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -27,10 +27,12 @@ router.get('/', withAuth, (req, res) => {
         },
       ],
     })
-    .then(() => {
-      
+    .then((dbPostData) => {
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true, username: req.session.username,});
       })
       .catch((err) => {
-
+        console.log(err);
+        res.status(500).json(err);
     });
 });
