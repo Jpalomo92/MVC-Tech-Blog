4d1a38fc74e9a7c7ebea09b1d47fcfa3e44549d8
Jpalomo <Jpalomo92@gmail.com>
2024-03-03 23:08:54 -0600

    fixed bugs in the connection

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
-const sequelize = require('../config/config');
+const sequelize = require('../config/connection');
 
 
 // Get all user's all posts onto their dashboard 
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
-const sequelize = require('../config/config');
+const sequelize = require('../config/connection');
 
 //not sure if I should pull in withAuth constant??
 
