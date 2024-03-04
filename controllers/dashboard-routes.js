fcacca8d26a1bb186d33bfa2383d18ee450c02b7
Jpalomo <Jpalomo92@gmail.com>
2024-03-03 21:56:58 -0600

    getting users posts to be on the dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,3 +2,9 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 const sequelize = require('../config/config');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+      where: {
+        userId: req.session.userId,
+      },
