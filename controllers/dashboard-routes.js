09605ee3404481a861c276ea08c2749a12631348
Jpalomo <Jpalomo92@gmail.com>
2024-03-03 21:58:18 -0600

    added a comment and need to look into what i need to include

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,8 +3,12 @@ const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 const sequelize = require('../config/config');
 
+
+// Get all user's all posts onto their dashboard 
 router.get('/', withAuth, (req, res) => {
     Post.findAll({
       where: {
         userId: req.session.userId,
       },
+      attributes: ['id', 'title', 'content', 'created_at'],
+      order: [['created_at', 'DESC']],
