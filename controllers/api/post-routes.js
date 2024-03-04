a0a61637f0772316162eeca7c63228fa63b4a8cc
Jpalomo <Jpalomo92@gmail.com>
2024-03-03 21:05:46 -0600

    working on post routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { User, Post, Comment  } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// create a new post ('/api/post')
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPost = await Post.create({ ...req.body, userId: req.session.userId });
+        console.log("This is the new post", newPost);
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
