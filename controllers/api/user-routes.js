ded21d27cff86ecc287fc1f7bd74cf5d2ad370e6
Jpalomo <Jpalomo92@gmail.com>
2024-03-03 20:55:43 -0600

    added if statements to check if login information is right - kept messaging the same so user will need to re-enter both email and password

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -16,3 +16,32 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+// this will allow the user to log in - if info incorrect it will let user know its not valid
+router.post('/login', async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            where: {username: req.body.username}
+        });
+        if (!dbUserData) {
+            res.status(400).json({ message: `User information is not valid.` });
+            return;
+        }
+        // check pw
+        const pwValidated = await dbUserData.checkPassword(req.body.password)
+        if (!pwValidated) {
+            res.status(400).json({ message: "User information is not valid." });
+            return;
+        }
+        // create session and send response back
+        req.session.save(() => {
+            req.session.userId = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;        
+        //send response to client
+        res.status(200).json({ message: "You are logged in!" });
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
