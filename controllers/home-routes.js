544f191b0ca87c5983debabdc2b4c090ac7b20cb
Jpalomo <Jpalomo92@gmail.com>
2024-03-03 21:44:35 -0600

    getting all posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,3 +3,10 @@ const { Post, User, Comment } = require('../models');
 const sequelize = require('../config/config');
 
 //not sure if I should pull in withAuth constant??
+// Get all posts ('/')
+router.get('/', async (req, res) => {
+    try {
+          // Retrieve all posts from db
+        const dbPostData = await Post.findAll({ 
+            attributes: ['id', 'title', 'content', 'created_at'],           
+            include:
