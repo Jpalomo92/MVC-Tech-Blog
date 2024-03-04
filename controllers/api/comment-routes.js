b4b61a9a3f6b0fb68788afdb6b33abee2cbd46ea
Jpalomo <Jpalomo92@gmail.com>
2024-03-03 21:25:12 -0600

    trying to get comments for only one post

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Comment, User, Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// get all comments api/comment
 router.get('/', async (req, res) => {
     try {
         const dbCommentData = await Comment.findAll({});
@@ -13,4 +14,20 @@ router.get('/', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+// create a route to get comment for only one post
+router.get('/:id', async (req, res) => {
+    try {
+        const CommentData = await Comment.findAll({
+            where:
+        });
+        if (dbCommentData.length === 0) {
+            res.status(404).json({ message: 'Please be sure to add content to your comment.'});
+            return;
+        };
+        res.status(200).json(dbCommentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
