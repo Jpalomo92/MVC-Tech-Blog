17d4fee4b2d7df84a2c485bbd90a53c3f41a35b7
Jpalomo <Jpalomo92@gmail.com>
2024-03-03 21:22:45 -0600

    added get route for comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
 const { Comment, User, Post } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const dbCommentData = await Comment.findAll({});
+        if (dbCommentData.length === 0) {
+            res.status(404).json({ message: 'Please be sure to add content to your comment.'});
+            return;
+        };
+        res.status(200).json(dbCommentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
